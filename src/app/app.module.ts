c123d487760e961bfd7cf5c9a1de684df46d4242
krishna Hari Shrestha <krishnashrestha49@gmail.com>
2019-11-01 23:11:11 +0545

    No provider for Http StaticInjectorError problem solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HomeComponent } from './components/site/home/home.component';
+
 import { RouterModule, Routes } from '@angular/router';
 
 import { BlogService } from './services/blog.service';
@@ -23,7 +25,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
   providers: [BlogService],
   bootstrap: [AppComponent]
